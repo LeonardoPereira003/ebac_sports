50c3f983b2d6cc9379971e84ab966fd75af0c8be
Leonardo <reisleo619@gmail.com>
2025-12-26 08:35:59 -0300

    Versão final

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,36 +1,36 @@
-  import { useDispatch } from 'react-redux'
-  import { useGetProductsQuery } from '../features/cart/api/apiSlice'
-  import { addItem } from '../features/cart/cartSlice'
-  import Produto from '../components/Produto'
-  import { Produto as ProdutoType } from '../types'
+import { useDispatch } from 'react-redux'
+import { useGetProductsQuery } from '../features/cart/api/apiSlice'
+import { addItem } from '../features/cart/cartSlice'
+import Produto from '../components/Produto'
+import { Produto as ProdutoType } from '../types'
 
-  const Produtos: React.FC = () => {
-    const dispatch = useDispatch()
-    const { data: products, isLoading, isError } = useGetProductsQuery()
+const Produtos: React.FC = () => {
+  const dispatch = useDispatch()
+  const { data: products, isLoading, isError } = useGetProductsQuery()
 
-    if (isLoading) return <p>Carregando...</p>
-    if (isError) return <p>Erro ao carregar produtos</p>
+  if (isLoading) return <p>Carregando...</p>
+  if (isError) return <p>Erro ao carregar produtos</p>
 
-    return (
-      <div>
-        {products?.map((p: ProdutoType) => (
-          <Produto
-            key={p.id}
-            produto={p}
-            onAddToCart={() =>
-              dispatch(
-                addItem({
-                  id: p.id,
-                  nome: p.nome,
-                  preco: p.preco,
-                  imagem: p.imagem
-                })
-              )
-            }
-          />
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {products?.map((p: ProdutoType) => (
+        <Produto
+          key={p.id}
+          produto={p}
+          onAddToCart={() =>
+            dispatch(
+              addItem({
+                id: p.id,
+                nome: p.nome,
+                preco: p.preco,
+                imagem: p.imagem
+              })
+            )
+          }
+        />
+      ))}
+    </div>
+  )
+}
 
-  export default Produtos
+export default Produtos
--- src/features/cart/api/apiSlice.ts
+++ src/features/cart/api/apiSlice.ts
@@ -1,16 +1,16 @@
-    import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-    import { Produto } from '../../../types'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Produto } from '../../../types'
 
-    export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://fake-api-tau.vercel.app/api/ebac_sports'
-    }),
-    endpoints: (builder) => ({
-        getProducts: builder.query<Produto[], void>({
-        query: () => '/produtos'
-        })
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api-ebac.vercel.app/api'
+  }),
+  endpoints: (builder) => ({
+    getProducts: builder.query<Produto[], void>({
+      query: () => 'ebac_sports/produtos'
     })
-    })
+  })
+})
 
-    export const { useGetProductsQuery } = apiSlice
+export const { useGetProductsQuery } = apiSlice
